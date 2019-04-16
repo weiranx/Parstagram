72f40c7d9ec30789ac4f6e5d6a7dc6223f2b759c
Weiran Xiong <weiranxiong@gmail.com>
2019-04-16 01:09:36 -0700

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Weiran Xiong on 4/16/19.
+//  Copyright © 2019 Weiran Xiong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
